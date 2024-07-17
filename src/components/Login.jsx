34f6d0f1826801c28a605f4a562e629729eba83f
simran <simranmanwani0@gmail.com>
2024-07-17 12:51:24 +0530

    build stage

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,20 +12,23 @@ const Login = ({ navigateTo }) => {
                     <h4 className='text-4xl font-semibold '>{item.title}</h4>
                     <p className='text-gray-400 mb-6'>{item.description}</p>
                     <p className='mb-2'>Email.Address</p>
-                    <input className='border-solid border-2 border-gray mb-5'
+                    <input className='border-solid rounded-sm border-2 border-gray mb-5'
                         type='email'
                         placeholder='Enter Email'
                     />
                     <p className='mb-2'>Password</p>
-                    <div className='flex'>
-                        <input className='border-solid border-2 border-gray'
+                    <div className='relative'>
+                        <input
+                            className='border-solid rounded-sm border-2 border-gray '
                             type='password'
                             placeholder='Enter Password'
                         />
-                        <FaRegEyeSlash className='' />
+                        <FaRegEyeSlash
+                            className='absolute right-1/3 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-500'
+                        />
                     </div>
 
-                    <p className='flex items-end justify-end text-orange-400 mb-10'>Forgot Password?</p>
+                    <p className='flex items-end justify-end text-orange-400 mt-4 mb-10'>Forgot Password?</p>
                     <div className='flex justify-center item-center'>
                         <button className='rounded-full text-white pr-16 pl-16 pt-3 pb-3 border-solid bg-orange-400 mb-10'
                             onClick={() => navigateTo('tracking')}>
@@ -38,8 +41,8 @@ const Login = ({ navigateTo }) => {
                         <p className='mx-4 mb-5'>or Sign In with</p>
                         <div className='flex-grow border-t border-gray-600'></div>
                     </div>
-                    <div className="flex justify-center items-center w-14 h-14 border-2 border-gray-400 rounded-full p-2 mt-3 mb-5">
-                        <FcGoogle className='text-4xl' />
+                    <div className="flex justify-center mx-auto items-center w-10 h-10 border-2 border-gray-400 rounded-full p-2 mt-3 mb-8">
+                        <FcGoogle className='text-4xl ' />
                     </div>
                     <div className='flex justify-center '>
                         <p className='text-black font-semibold mr-1'>{item.Bottom_heading}</p>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -8,7 +8,7 @@ const Register = () => {
         <div className=''>
             {
                 SignIn.map((item, index) => (
-                    <div key={index} className='p-5 mt-5 mb-5'>
+                    <div key={index} className='p-4 mt-5 mb-5'>
                         <h4 className='text-4xl font-semibold'>{item.title}</h4>
                         <p className='text-gray-400 mb-4'>{item.description}</p>
                         <p className='mb-2 '>Email.Address</p>
@@ -22,29 +22,29 @@ const Register = () => {
                             placeholder='Enter Username'
                         />
                         <p className='mb-2'>Password</p>
-                        <div className='flex  '>
+                        <div className='relative  '>
                             <input className='border-solid border-2 border-gray mb-5 w-80 h-10 p-4 rounded-lg'
                                 type='number'
                                 placeholder='Enter Password'
 
                             />
-                            <FaRegEyeSlash className='' />
+                            <FaRegEyeSlash className='absolute right-3 top-1/2 transform -translate-y-5 cursor-pointer text-gray-500' />
                         </div>
 
                         <div className='flex justify-center '>
-                            <p className='text-black font-semibold mr-1'>{item.Bottom_heading}</p>
+                            <p className='text-black font-semibold mr-1 mb-4'>{item.Bottom_heading}</p>
 
                         </div>
-                        <div className='flex justify-center item-center'><button className=' rounded-full text-white pr-16 pl-16 pt-3 pb-3 border-solid bg-orange-400 mb-10 w-72'>Register</button></div>
+                        <div className='flex justify-center item-center'><button className=' rounded-full text-white pr-16 pl-16 pt-3 pb-3 border-solid bg-orange-400 mb-5 w-72'>Register</button></div>
 
                         <div className='flex items-center justify-center'>
                             <div className='flex-grow border-t border-gray-600'></div>
                             <p className='mx-4 mb-5'>or Sign In with</p>
                             <div className='flex-grow border-t border-gray-600'></div>
-                            <FcGoogle className='text-4xl w-14 h-14 border-2  border-gray-400 rounded-full p-2' />
                         </div>
-                        {/* <div className="flex justify-end w-14 h-14 border-2 border-gray-400 rounded-full p-2"> */}
-                        {/* </div> */}
+                        <div className="flex justify-center mx-auto border-2 border-gray-400 rounded-full  h-10 w-10 mb-5">
+                            <FcGoogle className='text-4xl  ' />
+                        </div>
                         <div className='flex justify-center '>
                             <p className='text-black font-semibold mr-1'>{item.Bottom_subheading}</p>
                             <a className='text-orange-400 font-semibold' onClick={() => navigateTo('tracking')}> SignIn</a>
