90f79dab02cea7d2b2009295ce63be2c4aae872e
simran <simranmanwani0@gmail.com>
2024-07-17 17:40:03 +0530

    vite config changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+// vite.config.js
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  base: '/Internship_Project/', // Ensure this matches your GitHub repository name
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' ? '/Internship_Project/' : '/',
+    plugins: [react()],
+  }
 });
